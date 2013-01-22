cefa820c6541e4b79357203732180b29dd2e59c6
Oliver Garcia Albertos <oliverarafo@gmail.com>
2013-01-22 20:03:41 +0000

    Fix system/system cp of some blobs

--- golden-vendor-blobs.mk
+++ golden-vendor-blobs.mk
@@ -20,76 +20,76 @@ PRODUCT_COPY_FILES := \
 
 # Camera
 PRODUCT_COPY_FILES += \
-    vendor/samsung/golden/proprietary/system/lib/hw/camera.montblanc.so:system/system/lib/hw/camera.montblanc.so \
-    vendor/samsung/golden/proprietary/system/cameradata/datapattern_420sp.yuv:system/system/cameradata/datapattern_420sp.yuv \
-    vendor/samsung/golden/proprietary/system/cameradata/datapattern_front_420sp.yuv:system/system/cameradata/datapattern_front_420sp.yuv \
-    vendor/samsung/golden/proprietary/system/lib/libexifa.so:system/system/lib/libexifa.so \
-    vendor/samsung/golden/proprietary/system/lib/libjpega.so:system/system/lib/libjpega.so
+    vendor/samsung/golden/proprietary/system/lib/hw/camera.montblanc.so:system/lib/hw/camera.montblanc.so \
+    vendor/samsung/golden/proprietary/system/cameradata/datapattern_420sp.yuv:system/cameradata/datapattern_420sp.yuv \
+    vendor/samsung/golden/proprietary/system/cameradata/datapattern_front_420sp.yuv:system/cameradata/datapattern_front_420sp.yuv \
+    vendor/samsung/golden/proprietary/system/lib/libexifa.so:system/lib/libexifa.so \
+    vendor/samsung/golden/proprietary/system/lib/libjpega.so:system/lib/libjpega.so
 
 # Mali400 (GPU)
 PRODUCT_COPY_FILES += \
-    vendor/samsung/golden/proprietary/system/lib/egl/libEGL_mali.so:system/system/lib/egl/libEGL_mali.so \
-    vendor/samsung/golden/proprietary/system/lib/egl/libGLESv1_CM_mali.so:system/system/lib/egl/libGLESv1_CM_mali.so \
-    vendor/samsung/golden/proprietary/system/lib/egl/libGLESv2_mali.so:system/system/lib/egl/libGLESv2_mali.so \
-    vendor/samsung/golden/proprietary/system/lib/libMali.so:system/system/lib/libMali.so \
-    vendor/samsung/golden/proprietary/system/lib/libUMP.so:system/system/lib/libUMP.so
+    vendor/samsung/golden/proprietary/system/lib/egl/libEGL_mali.so:system/lib/egl/libEGL_mali.so \
+    vendor/samsung/golden/proprietary/system/lib/egl/libGLESv1_CM_mali.so:system/lib/egl/libGLESv1_CM_mali.so \
+    vendor/samsung/golden/proprietary/system/lib/egl/libGLESv2_mali.so:system/lib/egl/libGLESv2_mali.so \
+    vendor/samsung/golden/proprietary/system/lib/libMali.so:system/lib/libMali.so \
+    vendor/samsung/golden/proprietary/system/lib/libUMP.so:system/lib/libUMP.so
 
 # Display modules
 PRODUCT_COPY_FILES += \
-    vendor/samsung/golden/proprietary/system/lib/hw/hwcomposer.montblanc.so:system/system/lib/hw/hwcomposer.montblanc.so \
-    vendor/samsung/golden/proprietary/system/lib/hw/gralloc.montblanc.so:system/system/lib/hw/gralloc.montblanc.so
+    vendor/samsung/golden/proprietary/system/lib/hw/hwcomposer.montblanc.so:system/lib/hw/hwcomposer.montblanc.so \
+    vendor/samsung/golden/proprietary/system/lib/hw/gralloc.montblanc.so:system/lib/hw/gralloc.montblanc.so
 
 # Wifi firmware (bcm4334)
 PRODUCT_COPY_FILES += \
-    vendor/samsung/golden/proprietary/system/bin/bcm4334.hcd:system/system/bin/bcm4334.hcd \
-    vendor/samsung/golden/proprietary/system/etc/wifi/bcmdhd_apsta.bin_b2:system/system/etc/wifi/bcmdhd_apsta.bin_b2 \
-    vendor/samsung/golden/proprietary/system/etc/wifi/bcmdhd_mfg.bin_b2:system/system/etc/wifi/bcmdhd_mfg.bin_b2 \
-    vendor/samsung/golden/proprietary/system/etc/wifi/bcmdhd_p2p.bin_b2:system/system/etc/wifi/bcmdhd_p2p.bin_b2 \
-    vendor/samsung/golden/proprietary/system/etc/wifi/bcmdhd_sta.bin_b2:system/system/etc/wifi/bcmdhd_sta.bin_b2 \
-    vendor/samsung/golden/proprietary/system/etc/wifi/nvram_mfg.txt:system/system/etc/wifi/nvram_mfg.txt \
-    vendor/samsung/golden/proprietary/system/etc/wifi/nvram_net.txt:system/system/etc/wifi/nvram_net.txt
+    vendor/samsung/golden/proprietary/system/bin/bcm4334.hcd:system/bin/bcm4334.hcd \
+    vendor/samsung/golden/proprietary/system/etc/wifi/bcmdhd_apsta.bin_b2:system/etc/wifi/bcmdhd_apsta.bin_b2 \
+    vendor/samsung/golden/proprietary/system/etc/wifi/bcmdhd_mfg.bin_b2:system/etc/wifi/bcmdhd_mfg.bin_b2 \
+    vendor/samsung/golden/proprietary/system/etc/wifi/bcmdhd_p2p.bin_b2:system/etc/wifi/bcmdhd_p2p.bin_b2 \
+    vendor/samsung/golden/proprietary/system/etc/wifi/bcmdhd_sta.bin_b2:system/etc/wifi/bcmdhd_sta.bin_b2 \
+    vendor/samsung/golden/proprietary/system/etc/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
+    vendor/samsung/golden/proprietary/system/etc/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt
 
 # RIL
 PRODUCT_COPY_FILES += \
-    vendor/samsung/golden/proprietary/system/bin/rild:system/system/bin/rild \
-    vendor/samsung/golden/proprietary/system/lib/libril.so:system/system/lib/libril.so \
-    vendor/samsung/golden/proprietary/system/lib/libsec-ril.so:system/system/lib/libsec-ril.so \
-    vendor/samsung/golden/proprietary/system/lib/libsecril-client.so:system/system/lib/libsecril-client.so
+    vendor/samsung/golden/proprietary/system/bin/rild:system/bin/rild \
+    vendor/samsung/golden/proprietary/system/lib/libril.so:system/lib/libril.so \
+    vendor/samsung/golden/proprietary/system/lib/libsec-ril.so:system/lib/libsec-ril.so \
+    vendor/samsung/golden/proprietary/system/lib/libsecril-client.so:system/lib/libsecril-client.so
 
 # Sound --- TODO: Move alsa configs to prebuilt dir on device tree
 PRODUCT_COPY_FILES += \
-    vendor/samsung/golden/proprietary/system/lib/hw/audio.primary.DB8520H.so:system/system/lib/hw/audio.primary.DB8520H.so \
-    vendor/samsung/golden/proprietary/system/lib/hw/audio_policy.DB8520H.so:system/system/lib/hw/audio_policy.DB8520H.so \
-    vendor/samsung/golden/proprietary/system/lib/lib_Samsung_Resampler.so:system/system/lib/lib_Samsung_Resampler.so \
-    vendor/samsung/golden/proprietary/system/lib/libasound.so:system/system/lib/libasound.so \
-    vendor/samsung/golden/proprietary/system/lib/libsamsungSoundbooster.so:system/system/lib/libsamsungSoundbooster.so \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/alsa.conf:system/system/usr/share/alsa/alsa.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/cards/aliases.conf:system/system/usr/share/alsa/cards/aliases.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/center_lfe.conf:system/system/usr/share/alsa/pcm/center_lfe.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/default.conf:system/system/usr/share/alsa/pcm/default.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/dmix.conf:system/system/usr/share/alsa/pcm/dmix.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/dpl.conf:system/system/usr/share/alsa/pcm/dpl.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/dsnoop.conf:system/system/usr/share/alsa/pcm/dsnoop.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/front.conf:system/system/usr/share/alsa/pcm/front.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/iec958.conf:system/system/usr/share/alsa/pcm/iec958.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/modem.conf:system/system/usr/share/alsa/pcm/modem.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/rear.conf:system/system/usr/share/alsa/pcm/rear.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/side.conf:system/system/usr/share/alsa/pcm/side.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/surround40.conf:system/system/usr/share/alsa/pcm/surround40.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/surround41.conf:system/system/usr/share/alsa/pcm/surround41.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/surround50.conf:system/system/usr/share/alsa/pcm/surround50.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/surround51.conf:system/system/usr/share/alsa/pcm/surround51.conf \
-    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/surround71.conf:system/system/usr/share/alsa/pcm/surround71.conf \
+    vendor/samsung/golden/proprietary/system/lib/hw/audio.primary.DB8520H.so:system/lib/hw/audio.primary.DB8520H.so \
+    vendor/samsung/golden/proprietary/system/lib/hw/audio_policy.DB8520H.so:system/lib/hw/audio_policy.DB8520H.so \
+    vendor/samsung/golden/proprietary/system/lib/lib_Samsung_Resampler.so:system/lib/lib_Samsung_Resampler.so \
+    vendor/samsung/golden/proprietary/system/lib/libasound.so:system/lib/libasound.so \
+    vendor/samsung/golden/proprietary/system/lib/libsamsungSoundbooster.so:system/lib/libsamsungSoundbooster.so \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/alsa.conf:system/usr/share/alsa/alsa.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/cards/aliases.conf:system/usr/share/alsa/cards/aliases.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/center_lfe.conf:system/usr/share/alsa/pcm/center_lfe.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/default.conf:system/usr/share/alsa/pcm/default.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/dmix.conf:system/usr/share/alsa/pcm/dmix.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/dpl.conf:system/usr/share/alsa/pcm/dpl.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/dsnoop.conf:system/usr/share/alsa/pcm/dsnoop.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/front.conf:system/usr/share/alsa/pcm/front.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/iec958.conf:system/usr/share/alsa/pcm/iec958.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/modem.conf:system/usr/share/alsa/pcm/modem.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/rear.conf:system/usr/share/alsa/pcm/rear.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/side.conf:system/usr/share/alsa/pcm/side.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/surround40.conf:system/usr/share/alsa/pcm/surround40.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/surround41.conf:system/usr/share/alsa/pcm/surround41.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/surround50.conf:system/usr/share/alsa/pcm/surround50.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/surround51.conf:system/usr/share/alsa/pcm/surround51.conf \
+    vendor/samsung/golden/proprietary/system/usr/share/alsa/pcm/surround71.conf:system/usr/share/alsa/pcm/surround71.conf \
     vendor/samsung/golden/proprietary/system/bin/samsungpowersoundplay:system/bin/samsungpowersoundplay
 
 # GPS Module
 PRODUCT_COPY_FILES += \
-    vendor/samsung/golden/proprietary/system/lib/hw/gps.montblanc.so:system/system/lib/hw/gps.montblanc.so
+    vendor/samsung/golden/proprietary/system/lib/hw/gps.montblanc.so:system/lib/hw/gps.montblanc.so
 
 # Sensors/lights modules
 PRODUCT_COPY_FILES += \
-    vendor/samsung/golden/proprietary/system/lib/hw/lights.montblanc.so:system/system/lib/hw/lights.montblanc.so \
-    vendor/samsung/golden/proprietary/system/lib/hw/sensors.montblanc.so:system/system/lib/hw/sensors.montblanc.so
+    vendor/samsung/golden/proprietary/system/lib/hw/lights.montblanc.so:system/lib/hw/lights.montblanc.so \
+    vendor/samsung/golden/proprietary/system/lib/hw/sensors.montblanc.so:system/lib/hw/sensors.montblanc.so
 
 # OMX Codecs
 PRODUCT_COPY_FILES += \
@@ -166,7 +166,7 @@ PRODUCT_COPY_FILES += \
 
 # Misc stuff (aka idk what the hell is this used for)
 PRODUCT_COPY_FILES += \
-    vendor/samsung/golden/proprietary/system/etc/LVVEFS_tuning_parameters/LVVEFS_Rx_Configuration.txt:system/system/etc/LVVEFS_tuning_parameters/LVVEFS_Rx_Configuration.txt \
-    vendor/samsung/golden/proprietary/system/etc/LVVEFS_tuning_parameters/LVVEFS_Tx_Configuration.txt:system/system/etc/LVVEFS_tuning_parameters/LVVEFS_Tx_Configuration.txt \
-    vendor/samsung/golden/proprietary/system/etc/LVVEFS_tuning_parameters/Rx_ControlParams_SPEAKER_WB.txt:system/system/etc/LVVEFS_tuning_parameters/Rx_ControlParams_SPEAKER_WB.txt \
-    vendor/samsung/golden/proprietary/system/etc/LVVEFS_tuning_parameters/Tx_ControlParams_SPEAKER_WB.txt:system/system/etc/LVVEFS_tuning_parameters/Tx_ControlParams_SPEAKER_WB.txt
+    vendor/samsung/golden/proprietary/system/etc/LVVEFS_tuning_parameters/LVVEFS_Rx_Configuration.txt:system/etc/LVVEFS_tuning_parameters/LVVEFS_Rx_Configuration.txt \
+    vendor/samsung/golden/proprietary/system/etc/LVVEFS_tuning_parameters/LVVEFS_Tx_Configuration.txt:system/etc/LVVEFS_tuning_parameters/LVVEFS_Tx_Configuration.txt \
+    vendor/samsung/golden/proprietary/system/etc/LVVEFS_tuning_parameters/Rx_ControlParams_SPEAKER_WB.txt:system/etc/LVVEFS_tuning_parameters/Rx_ControlParams_SPEAKER_WB.txt \
+    vendor/samsung/golden/proprietary/system/etc/LVVEFS_tuning_parameters/Tx_ControlParams_SPEAKER_WB.txt:system/etc/LVVEFS_tuning_parameters/Tx_ControlParams_SPEAKER_WB.txt
