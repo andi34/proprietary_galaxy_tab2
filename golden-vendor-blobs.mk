a951d5d1e26c5d8734de56d7d8c42e91d9c407fb
OliverG96 <oliverarafo@gmail.com>
2013-11-09 18:03:21 +0000

    Remove spammer modem blobs

--- golden-vendor-blobs.mk
+++ golden-vendor-blobs.mk
@@ -94,9 +94,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/golden/proprietary/bin/cspsa-server:system/bin/cspsa-server \
     vendor/samsung/golden/proprietary/bin/msa:system/bin/msa \
     vendor/samsung/golden/proprietary/bin/at_core:system/bin/at_core \
-    vendor/samsung/golden/proprietary/bin/at_distributor:system/bin/at_distributor \
-    vendor/samsung/golden/proprietary/bin/modem-supervisor:system/bin/modem-supervisor \
-    vendor/samsung/golden/proprietary/bin/modem_log_relay:system/bin/modem_log_relay
+    vendor/samsung/golden/proprietary/bin/at_distributor:system/bin/at_distributor
 
 # System STE Libs
 PRODUCT_COPY_FILES += \
